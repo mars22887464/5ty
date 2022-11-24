90e476e85a7f34087392bb83b407616430b44b5b
mars22887464 <117431512+mars22887464@users.noreply.github.com>
2022-11-24 23:02:16 +1000

    Add files via upload

---
+++ 5BEn.py
@@ -0,0 +1,17 @@
+a1=56720
+b1=0.35
+print(a1-(a1*b1))
+
+c=int(input('Сколько у вас денег?'))
+print('Введите число'+':''{}'.format(c))
+print(c % 10)
+print(c % 100//10)
+print(c % 1000//100)
+print(c % 100000//1000)
+
+a=int(input())
+b=int(input())
+p=(a+b)**2
+i=(a**2)+(b**2)
+print(p)
+print(i)
